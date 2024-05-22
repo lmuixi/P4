f09bb74795e43077e6362d5b90d9fd01276eb60a
lmuixi <laura.muixi@estudiantat.upc.edu>
2024-05-22 16:11:22 +0200

    prova

--- scripts/run_spkid.sh
+++ scripts/run_spkid.sh
@@ -87,6 +87,27 @@ compute_lp() {
     done
 }
 
+compute_lpcc(){
+    db=$1
+    shift
+    for filename in $(sort $*); do
+        mkdir -p `dirname $w/$FEAT/$filename.$FEAT`
+        EXEC="wav2lpcc 25 25 $db/$filename.wav $w/$FEAT/$filename.$FEAT"
+        echo $EXEC && $EXEC || exit 1
+    done
+}
+
+compute_mfcc(){
+    db=$1
+    shift
+    for filename in $(sort $*); do
+        mkdir -p `dirname $w/$FEAT/$filename.$FEAT`
+        EXEC="wav2mfcc 8 20 33 $db/$filename.wav $w/$FEAT/$filename.$FEAT"
+        echo $EXEC && $EXEC || exit 1
+    done
+
+}
+
 #  Set the name of the feature (not needed for feature extraction itself)
 if [[ ! -v FEAT && $# > 0 && "$(type -t compute_$1)" = function ]]; then
     FEAT=$1
@@ -174,7 +195,9 @@ for cmd in $*; do
        #
        # El fichero con el resultado del reconocimiento debe llamarse $FINAL_CLASS, que deberá estar en el
        # directorio de la práctica (PAV/P4).
-       echo "To be implemented ..."
+       compute_$FEAT $db_test $lists/final/class.test
+       EXEC="gmm_classify -d $w/$FEAT -e $FEAT -D $w/gmm/$FEAT -E gmm $lists/gmm.list $lists/final/class.test"
+       echo $EXEC && $EXEC | tee $FINAL_CLASS || exit 1
    
    elif [[ $cmd == finalverif ]]; then
        ## @file
@@ -193,7 +216,12 @@ for cmd in $*; do
        # candidato para la señal a verificar. En $FINAL_VERIF se pide que la tercera columna sea 1,
        # si se considera al candidato legítimo, o 0, si se considera impostor. Las instrucciones para
        # realizar este cambio de formato están en el enunciado de la práctica.
-       echo "To be implemented ..."
+       compute_$FEAT $db_test $lists/final/verif.test
+       EXEC="gmm_verify -d $w/$FEAT -e $FEAT -D $w/gmm/$FEAT/ -w $world -E gmm lists/gmm.list $lists/final/verif.test $lists/final/verif.test.candidates"
+       echo $EXEC && $EXEC | tee $TEMP_VERIF || exit 1
+       perl -ane 'print "$F[0]\t$F[1]\t";
+            if ($F[2] > -3.214) {print "1\n"}
+            else {print "0\n"}' $TEMP_VERIF | tee $FINAL_VERIF
    
    # If the command is not recognize, check if it is the name
    # of a feature and a compute_$FEAT function exists.
---
+++ scripts/wav2mfcc.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Base name for temporary files
+base=/tmp/$(basename $0).$$ 
+
+# Ensure cleanup of temporary files on exit
+trap cleanup EXIT
+cleanup() {
+   \rm -f $base.*
+}
+
+if [[ $# != 5 ]]; then
+   echo "$0 fm mfcc_order melbank_order input.wav output.mfcc"
+   exit 1
+fi
+
+fm=$1
+mfcc_order=$2
+melbank_order=$3
+inputfile=$4
+outputfile=$5
+
+
+if [[ $UBUNTU_SPTK == 1 ]]; then
+   # In case you install SPTK using debian package (apt-get)
+   X2X="sptk x2x"
+   FRAME="sptk frame"
+   WINDOW="sptk window"
+   MFCC="sptk mfcc"
+else
+   # or install SPTK building it from its source
+   X2X="x2x"
+   FRAME="frame"
+   WINDOW="window"
+   MFCC="mfcc"
+fi
+
+# Main command for feature extration
+sox $inputfile -t raw -e signed -b 16 - | $X2X +sf | $FRAME -l 240 -p 80 | $WINDOW -l 240 -L 240 |
+	$MFCC -s $fm -l 240 -m $mfcc_order -n $melbank_order > $base.mfcc
+
+# Our array files need a header with the number of cols and rows:
+ncol=$((mfcc_order)) # mfcc p =>  (a0 a1 a2 ... ap-1) 
+nrow=`$X2X +fa < $base.mfcc | wc -l | perl -ne 'print $_/'$ncol', "\n";'`
+
+# Build fmatrix file by placing nrow and ncol in front, and the data after them
+echo $nrow $ncol | $X2X +aI > $outputfile
+cat $base.mfcc >> $outputfile
+
+exit
